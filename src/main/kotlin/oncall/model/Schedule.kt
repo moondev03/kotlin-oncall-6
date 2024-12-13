c66b18504383a3d906988e76c3dfd91ff3d0ae96
moondev03 <moondev03@gmail.com>
2024-12-13 21:42:28 +0900

    feat(Model): Schedule 객체 구현
    - 하루 스케줄 정보를 관리
    - month, date, day, isHoliday, worker 인자 가짐

---
+++ src/main/kotlin/oncall/model/Schedule.kt
@@ -0,0 +1,25 @@
+package oncall.model
+
+import oncall.model.Days.Companion.isHoliday
+import oncall.utils.LegalHoliday
+
+data class Schedule(
+    val month: Int,
+    val date: Int,
+    val day: Days,
+    var isHoliday: Boolean = false,
+    val worker: String? = null
+) {
+    init {
+        require(month in 1..12) { "month는 1~12 사이의 정수여야 합니다." }
+        require(date in 1..if(month != 2) 31 else 28) { "date는 1~31 사이의 정수여야 합니다. (2월은 28일까지)" }
+
+        checkIsHoliday()
+    }
+
+    private fun checkIsHoliday() {
+        val isHoliday = day.isHoliday()
+        val isLegalHoliday = LegalHoliday.isHoliday(month, date)
+        this.isHoliday = isHoliday || isLegalHoliday
+    }
+}
